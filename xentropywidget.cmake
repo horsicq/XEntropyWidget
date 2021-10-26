7799b37d5aae4ad5d0cab2d06a4426d38031a9bd
horsicq <horsicq@gmail.com>
2021-10-27 00:38:31 +0200

    Fix: 2021-10-27

--- xentropywidget.cmake
+++ xentropywidget.cmake
@@ -1,5 +1,7 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
+# TODO
+
 set(XENTROPYWIDGET_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/dialogentropyprocess.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xentropywidget.cpp
