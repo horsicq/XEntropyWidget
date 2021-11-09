4a51380f7a6712655a7fd1deda69e94f8ca4a5c7
horsicq <horsicq@gmail.com>
2021-11-09 19:37:41 +0100

    Fix: 2021-11-09

--- xentropywidget.cmake
+++ xentropywidget.cmake
@@ -8,11 +8,10 @@ set(XENTROPYWIDGET_SOURCES
     ${ENTROPYPROCESS_SOURCES}
     ${XQWT_SOURCES}
     ${XLINEEDITHEX_SOURCES}
-    ${CMAKE_CURRENT_LIST_DIR}/dialogentropyprocess.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/xentropywidget.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogentropy.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/dialogentropyprocess.ui
-    ${CMAKE_CURRENT_LIST_DIR}/xentropywidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogentropy.ui
-
+    ${CMAKE_CURRENT_LIST_DIR}/dialogentropyprocess.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogentropyprocess.ui
+    ${CMAKE_CURRENT_LIST_DIR}/xentropywidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xentropywidget.ui
 )
