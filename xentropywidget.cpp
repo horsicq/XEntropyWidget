c154b6adcf9a25ee471b0d7c4caf6f99ea2149b6
horsicq <horsicq@gmail.com>
2022-11-02 02:30:10 +0100

    Fix: 2022-11-02

--- xentropywidget.cpp
+++ xentropywidget.cpp
@@ -42,8 +42,6 @@ XEntropyWidget::XEntropyWidget(QWidget *pParent) :
     g_nOffset=0;
     g_nSize=0;
 
-    g_pGrid=nullptr; // TODO remove TODO Check
-
     g_pGrid=new QwtPlotGrid;
     g_pGrid->enableXMin(true);
 
