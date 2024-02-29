daf29430c6e1e1ed59c8bc8997240d2e2966f823
horsicq <horsicq@gmail.com>
2024-03-01 00:08:01 +0100

    Update file(s): 2024-03-01

--- entropyprocess.cmake
+++ entropyprocess.cmake
@@ -5,4 +5,5 @@ include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
 set(ENTROPYPROCESS_SOURCES
     ${XFORMATS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/entropyprocess.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/entropyprocess.h
     )
