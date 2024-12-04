f40e97fcff8dfadad8b6eea8431e03823054e009
horsicq <horsicq@gmail.com>
2024-12-05 00:33:04 +0100

    Update file(s): 2024-12-05

--- entropyprocess.cmake
+++ entropyprocess.cmake
@@ -1,8 +1,12 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
+if (NOT DEFINED XFORMATS_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
+    set(ENTROPYPROCESS_SOURCES ${ENTROPYPROCESS_SOURCES} ${XFORMATS_SOURCES})
+endif()
 
 set(ENTROPYPROCESS_SOURCES
+    ${ENTROPYPROCESS_SOURCES}
     ${XFORMATS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/entropyprocess.cpp
     ${CMAKE_CURRENT_LIST_DIR}/entropyprocess.h
